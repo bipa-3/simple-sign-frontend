8a599bcabe8967b8a349248362ce108ff7fdab70
jmlbw <144968527+jmlbw@users.noreply.github.com>
2023-10-18 10:23:15 +0900

    test: https login test

--- src/apis/loginAPI/postLogin.js
+++ src/apis/loginAPI/postLogin.js
@@ -1,5 +1,11 @@
 import axios from 'axios';
 import base_url from '../base_url';
+const https = require('https');
+const fs = require('fs');
+
+const agent = new https.Agent({
+  ca: fs.readFileSync('../../ca-certificate.pem'), // CA 또는 루트 인증서 파일
+});
 
 const api = axios.create({
   baseURL: base_url,
@@ -8,6 +14,7 @@ const api = axios.create({
 
 export const postLogin = (loginId, password) => {
   return api.post('/login', {
+    httpsAgent: agent,
     loginId,
     password,
   });
